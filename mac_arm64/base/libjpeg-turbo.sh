7e9750c7e71bfece70efa44a8ba29b4d716b0fb2
xianyunleo <xianyunleo@gmail.com>
2026-06-20 16:27:07 +0800

    update

--- mac_arm64/base/libjpeg-turbo.sh
+++ mac_arm64/base/libjpeg-turbo.sh
@@ -35,7 +35,6 @@ cd build
 # -------------------------------
 args=(
   "-DCMAKE_INSTALL_PREFIX=$PREFIX"
-  "-DCMAKE_SKIP_RPATH=ON"
   "-DCMAKE_BUILD_TYPE=Release"
   "-DCMAKE_FIND_FRAMEWORK=LAST"
   "-DCMAKE_VERBOSE_MAKEFILE=ON"
