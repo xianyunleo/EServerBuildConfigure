d43115c4c64335c702b84bcf45ba5c737156024c
xianyunleo <xianyunleo@gmail.com>
2026-06-24 18:18:17 +0800

    update

--- mac_arm64/php/php-8.5.copy.sh
+++ mac_arm64/php/php-8.5.copy.sh
@@ -42,9 +42,43 @@ make clean || true
 # 配置
 # -------------------------------
 
+export SDKROOT=$(xcrun --sdk macosx --show-sdk-path)
+export CFLAGS="-isysroot $SDKROOT"
+export CPPFLAGS="-isysroot $SDKROOT"
+export LDFLAGS="-isysroot $SDKROOT"
+export PKG_CONFIG_PATH=/Applications/EServer/Library/openssl@3.5/lib/pkgconfig:/Applications/EServer/Library/curl/lib/pkgconfig:/Applications/EServer/Library/libgd/lib/pkgconfig:/Applications/EServer/Library/oniguruma/lib/pkgconfig:/Applications/EServer/Library/zlib/lib/pkgconfig:/Applications/EServer/Library/libxml2/lib/pkgconfig:/Applications/EServer/Library/libzip/lib/pkgconfig:/Applications/EServer/Library/icu/lib/pkgconfig${PKG_CONFIG_PATH:+:$PKG_CONFIG_PATH}
 
 ./configure --prefix="$PREFIX" \
-  --with-config-file-path="$PREFIX/etc"
+  --with-config-file-path="$PREFIX/etc" \
+  --enable-bcmath \
+  --enable-calendar \
+  --enable-exif \
+  --enable-ftp \
+  --enable-fpm \
+  --enable-gd=shared \
+  --with-external-gd \
+  --enable-mbstring \
+  --enable-mbregex \
+  --enable-opcache \
+  --enable-soap \
+  --enable-sockets \
+  --enable-intl \
+  --enable-pcntl \
+  --with-bz2=/Applications/EServer/Library/bzip2 \
+  --with-curl=shared \
+  --with-gmp=/Applications/EServer/Library/gmp \
+  --with-iconv=/Applications/EServer/Library/libiconv \
+  --with-mysqli \
+  --with-openssl=shared \
+  --with-pdo-mysql \
+  --with-pgsql=/Applications/EServer/Library/libpq \
+  --with-pdo-pgsql=/Applications/EServer/Library/libpq \
+  --with-pdo-sqlite \
+  --with-sqlite3 \
+  --with-libxml \
+  --with-zip \
+  --with-zlib
+
 # -------------------------------
 # 编译安装
 # -------------------------------
